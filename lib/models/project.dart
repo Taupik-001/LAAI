b3b9a0b9aa08debece0c7246631af47b4584ad8b
Taupik-001 <taufikbjm24@gmail.com>
2023-07-01 07:23:53 +0800

    Start up set up, for code

---
+++ lib/models/project.dart
@@ -0,0 +1,27 @@
+class Project {
+  final String? title, description;
+  Project({this.title, this.description});
+}
+
+List<Project> demoProjects = [
+  Project(
+      title: "Responsive Admin Panel or Dashboard - Flutter UI",
+      description:
+          "Responsive Admin Panel or Dashboard - Flutter UI, Responsive Admin Panel or Dashboard - Flutter UI. Responsive Admin Panel or Dashboard - Flutter UI."),
+  Project(
+      title: "Responsive Admin Panel or Dashboard - Flutter UI",
+      description:
+          "Responsive Admin Panel or Dashboard - Flutter UI, Responsive Admin Panel or Dashboard - Flutter UI. Responsive Admin Panel or Dashboard - Flutter UI."),
+  Project(
+      title: "Responsive Admin Panel or Dashboard - Flutter UI",
+      description:
+          "Responsive Admin Panel or Dashboard - Flutter UI, Responsive Admin Panel or Dashboard - Flutter UI. Responsive Admin Panel or Dashboard - Flutter UI."),
+  Project(
+      title: "Responsive Admin Panel or Dashboard - Flutter UI",
+      description:
+          "Responsive Admin Panel or Dashboard - Flutter UI, Responsive Admin Panel or Dashboard - Flutter UI. Responsive Admin Panel or Dashboard - Flutter UI."),
+  Project(
+      title: "Responsive Admin Panel or Dashboard - Flutter UI",
+      description:
+          "Responsive Admin Panel or Dashboard - Flutter UI, Responsive Admin Panel or Dashboard - Flutter UI. Responsive Admin Panel or Dashboard - Flutter UI."),
+];
---
+++ lib/models/recomendation.dart
@@ -0,0 +1,37 @@
+class Recomendation {
+  final String? name, source, text;
+  Recomendation({this.name, this.source, this.text});
+}
+
+final List<Recomendation> demoRecomendations = [
+  Recomendation(
+      name: "Kalopa Obanewa",
+      source: "Linkedin",
+      text:
+          "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he. I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he."),
+  Recomendation(
+      name: "Kalopa Obanewa",
+      source: "Linkedin",
+      text:
+          "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he. I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he."),
+  Recomendation(
+      name: "Kalopa Obanewa",
+      source: "Linkedin",
+      text:
+          "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he. I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he."),
+  Recomendation(
+      name: "Kalopa Obanewa",
+      source: "Linkedin",
+      text:
+          "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he. I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he."),
+  Recomendation(
+      name: "Kalopa Obanewa",
+      source: "Linkedin",
+      text:
+          "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he. I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he."),
+  Recomendation(
+      name: "Kalopa Obanewa",
+      source: "Linkedin",
+      text:
+          "I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he. I have had the pleasure of working with Anwar on two flutter projects and what strikes me the most about he."),
+];
